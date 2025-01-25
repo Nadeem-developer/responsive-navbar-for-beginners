e4471465c57c2017e6578136f4842916fd98738c
Khan Nadeem Ahmed <nadeem5278652@gmail.com>
2025-01-25 15:13:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector("#menu").addEventListener("click", function () {
+  document.querySelector(".sandwitch").classList.toggle("sandwitch1");
+  document.querySelector("#menu").classList.toggle("menueRotate");
+});
